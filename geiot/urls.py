20b35cfb4bbe2c08fac797b88ff54f137f520fe3
techtake <27718637+techtake@users.noreply.github.com>
2020-07-16 16:56:20 +0900

    made the top page

---
+++ geiot/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+
+
+# アプリ名を記述
+app_name = 'geiot'
+
+# ここの配列の中にルーティングを書いていきます。
+urlpatterns = [
+  path('', views.index, name="index"),
+]
